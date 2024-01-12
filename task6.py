130f8e33abd331adde7cb39a6c54c9df90b95467
patrickparseg <patrickparseg23@gmail.com>
2024-01-11 17:33:02 -0800

    updated camera functions for task 6

---
+++ task6.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_color = np.array([30, 50, 50])
+    upper_color = np.array([60,255,255])
+
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        largest_contour = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+        
+
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
